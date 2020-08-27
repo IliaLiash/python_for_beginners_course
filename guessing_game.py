2b60a09617c82902948c9c62aad072f97d6be63c
IliaLiash <70135336+IliaLiash@users.noreply.github.com>
2020-08-27 13:31:56 +0300

    Add files via upload

---
+++ guessing_game.py
@@ -0,0 +1,51 @@
+from random import*
+new_game = True
+
+while new_game:
+
+    print('Welcome to quessing game!')
+    print('Enter the border "n" [1, n]:', end=' ')
+
+    n = int(input())
+    num = randint(1,n)
+
+    def is_valid(n):
+        return 1 <= n <= 100
+
+    print('Enter your number:', end=' ')
+    find_number = int(input())
+    count = 0
+
+    while find_number != num:
+    
+        if is_valid(find_number):
+            if find_number < num:
+                print('Your number is less than the guessed one, try again')
+                find_number = int(input())
+                count += 1
+            elif find_number > num:
+                print('Your number is more than the guessed one, try again')
+                find_number = int(input())
+                count += 1
+               
+        else:
+            print('Maybe enter integer from 1 to ', n, '?', sep='')
+            find_number = int(input())
+            count += 1
+
+    if find_number == num:
+        print('Yes! It is ', num, '!', sep='')
+        print('Congrats, You guessed that number on the', count + 1, 'try!!')
+        print('Do you wanna try once more? (Y, N)')
+    
+    answer = input()
+    if answer == 'Y':
+        new_game = True
+        print('-' * 25)  
+    else:
+        new_game = False
+        print('Thanks for playing the guessing game!')
+
+     
+
+
